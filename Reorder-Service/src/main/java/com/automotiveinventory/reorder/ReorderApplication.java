e6c393190127718c0e3af5919d3da455c5e9189f
Ragu S <raguramesh252004@gmail.com>
2026-09-10 21:46:40 +0530

    feat: created  reorder-service and inventory-ops-service

---
+++ Reorder-Service/src/main/java/com/automotiveinventory/reorder/ReorderApplication.java
@@ -0,0 +1,13 @@
+package com.automotiveinventory.reorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReorderApplication.class, args);
+	}
+
+}
