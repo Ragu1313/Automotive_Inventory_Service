73e212e21e7dc913f2fa36fa97a75f3a97d4911f
nithish0302 <nithish2005palani@gmail.com>
2026-09-12 12:20:58 +0530

    feat: setup login page

--- MotoVault/vite.config.js
+++ MotoVault/vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+
+    hmr: {
+      clientPort: 443,
+    },
+  },
 });
